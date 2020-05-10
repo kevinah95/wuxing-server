fb35bf3f4ffc2f6a50896c775eaf8544ebd9a119
Jose Pablo Araya <araya.josepablo@gmail.com>
2020-05-09 20:04:25 -0600

    Code refactor

--- common.h
+++ common.h
@@ -2,13 +2,10 @@
 
 #define BUFFER_SIZE 1024 // Fixed buffer size
 
-// Servers ports
-#define FIFO_PORT 51717
-#define THREAD_PORT 51718
-#define PRE_THREAD_PORT 51719
-
 #define OK_HTTP 200             // Success response
 const int HTTP_NOT_FOUND = 404; // File not found response
+
+#define PRE_THREAD_PORT 51720
 
 const char *FILE_NOT_FOUND = "file_not_found.html";
 
---
+++ socketLib/socket_lib.h
@@ -0,0 +1,8 @@
+extern int saccept ();
+extern int slisten ();
+
+// Servers ports
+#define FIFO_PORT 51717
+#define FORK_PORT 51718
+#define THREAD_PORT 51719
+//#define PRE_THREAD_PORT 51720
